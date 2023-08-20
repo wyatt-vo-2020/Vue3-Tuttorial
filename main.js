274c1919d59c4c6199cb27f0461c62e62274b4f0
Wyatt VO <votranquy96@gmail.com>
2023-08-21 01:10:17 +0700

    Move content to template in createApp

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 const app = Vue.createApp({
+    template:`<h1>Xin chao {{teamName}} and {{firstName}} {{lastName}}</h1>
+    <button v-on:click="onChangeName">Change Name 1</button>
+    <button @click="onChangeName">Change Name 2</button>
+    <div class="modal" v-if="isShowModel">
+        <h1>Modal content</h1>
+    </div>`,
     data() {
         return {
             teamName: "ABC",
             firstName: "Vo",
             lastName: "Quy",
-            isHello: false
+            isHello: false,
+            isShowModel: false
         };
     },
     methods: {
         onChangeName(){
-            console.log(this);
             this.firstName = "Merlin",
-            this.lastName = "Wyatt"
+            this.lastName = "Wyatt",
+            this.isShowModel = !this.isShowModel
         },
 
     }
